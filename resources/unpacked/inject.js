4f4d57ca605f944613b79ca69ab5f96e6c6b2e46
Lih Chen <lihster@gmail.com>
2020-04-19 19:24:21 -0700

    clean up

--- resources/unpacked/inject.js
+++ resources/unpacked/inject.js
@@ -1,5 +1,3 @@
-console.log("hello! from inject");
-
 function inject(){
   var injectionScript = document.createElement('script');
   injectionScript.type = 'text/javascript';
@@ -31,12 +29,6 @@ console.log(">> inside load: this: ", this);
 
   document.head.prepend(injectionScript);
 }
-
-// [["wrb.fr","mE1oA","[\"https://polymorphiclabs.io/\",[[\"0000000000000000/110687702074834780045/0005a397878c102a/srv-is\",\"https://polymorphiclabs.io/foo\",1,1587246093046914,1,1,false,0]\
-
-// ["wrb.f["wrb.fr","vm16b",null,null,null,[6,null,[["type.googleapis.com/social.frontend.searchconsole.data.SubmitRemovalError",[3]↵]↵]↵]↵,"generic"]↵,["di",292]↵,["af.httprm",291,"-3391450240536993335",30]↵]↵26↵[["e",4,null,null,239]r","vm16b",null,null,null,[6,null,[["type.googleapis.com/social.frontend.searchconsole.data.SubmitRemovalError",[3]↵]↵]↵]↵,"generic"]↵,["di",292]↵,["af.httprm",291,"-3391450240536993335",30]↵]↵26↵[["e",4,null,null,239]
-
-// response: ")]}'↵↵112↵[["wrb.fr","vm16b","[]\n",null,null,null,"generic"]↵,["di",261]↵,["af.httprm",260,"-7164925716848719425",28]↵]↵26↵[["e",4,null,null,149]↵]↵"
 
 
 document.addEventListener("DOMContentLoaded", function(){
