299dfa3443aaebc525a6044a9f2fbfbb02a6568f
Lih Chen <lihster@gmail.com>
2020-09-26 23:22:42 -0700

    Handles the DOS ^M character

--- src/popup/google_webmaster_tools_bulk_url_removal/popup/core.cljs
+++ src/popup/google_webmaster_tools_bulk_url_removal/popup/core.cljs
@@ -170,6 +170,14 @@
     (go-loop []
       (let [file-content (<! read-chan)
             _ (prn "file-content: " (clojure.string/trim file-content))
+            file-content (->> (-> file-content
+                                  ;; split by ^M character
+                                  (clojure.string/split #"\r"))
+                              (map clojure.string/trim)
+                              (remove (fn [x]
+                                        (zero? (count x))))
+                              (clojure.string/join "\n"))
+            _ (prn "sanitized file-content: " (clojure.string/trim file-content))
             csv-data (->> (csv/read-csv (clojure.string/trim file-content))
                           ;; trim off random whitespaces
                           (map (fn [[url method url-type]]
